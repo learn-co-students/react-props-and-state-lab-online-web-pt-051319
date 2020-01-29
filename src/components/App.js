9dde8ca6b003c3578c5f509d181a6c676cdbfd0f
jamievullo <jvullo@protonmail.com>
2020-01-29 13:24:20 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,32 @@ class App extends React.Component {
     }
   }
 
+  fetchPets = () => {
+    let url = '/api/pets';
+    //checks input of scroll and concats to end of default all pets url with exact route descriped in fetch-setup
+    if (this.state.filters.type !== 'all') {
+      url += `?type=${this.state.filters.type}`;
+    }
+    //passes in onchangetype url to regular fetch command
+    fetch(url)
+    .then(response => response.json())
+    .then(pets => this.setState({pets: pets}));
+  };
+
+  onChangeType = ({ target: { value } }) => {
+      //somehow when the user picks type of pet, get the changes value? and set state to the value chosen?
+      this.setState({filters: { ...this.state.filters, type: value }});
+      // console.log({filters: {...this.state.filters, type: value}})
+      // console.log({target: {value}})
+  };
+
+  onAdoptPet = petId => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? { ...p, isAdopted: true } : p;
+    });
+    this.setState({ pets: pets });
+  };
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +50,16 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters 
+              onChangeType={this.onChangeType}
+              onFindPetsClick={this.fetchPets}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser 
+              pets={this.state.pets} 
+              onAdoptPet={this.onAdoptPet}
+              />
             </div>
           </div>
         </div>
